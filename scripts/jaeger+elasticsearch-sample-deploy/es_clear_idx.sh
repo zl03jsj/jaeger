ec2172a772c6ad8617a9c4123893d919f7c5e25f
zl <zl03jsj@163.com>
2021-04-22 15:44:26 +0800

    tool for running example

---
+++ scripts/jaeger+elasticsearch-sample-deploy/es_clear_idx.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+ES_URL_AND_PORT=192.168.1.189:9200
+LAST_DATE=`date "+%Y.%m.%d"`
+
+if [ "$(uname)" == "Darwin" ] ;then
+  LAST_DATE=`date -v-0d "+%Y.%m.%d"`
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ];then
+  LAST_DATE=`date -d "-0 days" "+%Y.%m.%d"`
+fi
+
+main() {
+  curl -XDELETE 'http://'$ES_URL_AND_PORT'/*-'${LAST_DATE}'-*?pretty'
+
+  curl -H'Content-Type:application/json' -d'{
+"query": { "range": { "startTimeMillis": {"lt":"now-20m", "format":"epoch_millis"} } }
+}' -XPOST "$ES_URL_AND_PORT/*-*/_delete_by_query?pretty"
+}
+
+main "$@"
